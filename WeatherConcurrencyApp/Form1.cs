90599dfe7e6b1baf0ee706f34316a5eddde66793
AbdielGE <86158145+AbdielGarc1a@users.noreply.github.com>
2022-05-05 16:19:24 -0600

    Add files via upload

---
+++ WeatherConcurrencyApp/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeatherConcurrencyApp.Infrastructure.OpenWeatherClient;
+using WeatherConcurrentApp.Domain.Entities;
+
+namespace WeatherConcurrencyApp
+{
+    public partial class FrmMain : Form
+    {
+        public HttpOpenWeatherClient httpOpenWeatherClient;
+        public OpenWeather openWeather;
+        public FrmMain()
+        {
+            httpOpenWeatherClient = new HttpOpenWeatherClient();
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Task.Run(Request).Wait();
+                if(openWeather == null)
+                {
+                    throw new NullReferenceException("Fallo al obtener el objeto OpeWeather.");
+                }
+
+                WeatherPanel weatherPanel = new WeatherPanel();
+                weatherPanel.pictureBox1.ImageLocation = "https://openweathermap.org/img/w/" + openWeather.Weather[0].Icon + ".png";
+                weatherPanel.lblCity.Text = textBox1.Text;
+                weatherPanel.lblTemperature.Text = openWeather.Main.Temp.ToString() + "°C";
+                weatherPanel.lblWeather.Text = "Temperature";
+                weatherPanel.feels.lblDetailValue.Text = openWeather.Main.Feels_like.ToString();
+              
+                weatherPanel.presion.lblDetailValue.Text = openWeather.Main.Pressure.ToString();
+                weatherPanel.humedad.lblDetailValue.Text = openWeather.Main.Humidity.ToString();
+                weatherPanel.tempMin.lblDetailValue.Text = openWeather.Main.Temp_min.ToString() + "°C";
+                weatherPanel.tempMax.lblDetailValue.Text = openWeather.Main.Temp_max.ToString() + "°C";
+                weatherPanel.longitud.lblDetailValue.Text = openWeather.Coord.Lon.ToString();
+                weatherPanel.latitud.lblDetailValue.Text = openWeather.Coord.Lat.ToString();
+                
+                weatherPanel.tmzone.lblDetailValue.Text = openWeather.Timezone.ToString();
+                weatherPanel.velocidad.lblDetailValue.Text = openWeather.Wind.Speed.ToString();
+                weatherPanel.visibilidad.lblDetailValue.Text = openWeather.Visibility.ToString() + "m";
+                weatherPanel.sunr.lblDetailValue.Text = convertLongToDate(openWeather.Sys.Sunrise).ToShortTimeString();
+                weatherPanel.suns.lblDetailValue.Text = convertLongToDate(openWeather.Sys.Sunset).ToShortTimeString();
+                flpContent.Controls.Add(weatherPanel);
+
+            }
+            catch (Exception)
+            {
+                
+            }
+           
+        }
+        DateTime convertLongToDate(long date)
+        {
+            DateTime time = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).ToLocalTime();
+            time = time.AddSeconds(date).ToLocalTime();
+            return time;
+        }
+        public async Task Request()
+        {
+           openWeather = await httpOpenWeatherClient.GetWeatherByCityNameAsync(textBox1.Text);
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        //private void historial()
+        //{
+        //    FrmForecast frmForecast;
+        //    for (int i = 0; i < 8; i++)
+        //    {
+        //        frmForecast = new FrmForecast();
+        //        frmForecast.TopLevel = false;
+        //        frmForecast.pictureBox1.ImageLocation = wheaterser.GetImageLocation(forecast.daily[i].weather[0]);
+        //        frmForecast.lblDescription.Text = forecast.daily[i].weather[0].description;
+        //        frmForecast.lblWeather.Text = forecast.daily[i].weather[0].main;
+        //        frmForecast.lblDay.Text = wheaterser.convertToDateTime(forecast.daily[i].dt).DayOfWeek.ToString();
+        //        flowLayoutPanel1.Controls.Add(frmForecast);
+        //        frmForecast.Show();
+        //    }
+        //}
+    }
+}
---
+++ WeatherConcurrencyApp/WeatherPanel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherConcurrencyApp
+{
+    public partial class WeatherPanel : UserControl
+    {
+        public DetailsWeather longitud = new DetailsWeather();
+        public DetailsWeather latitud = new DetailsWeather();
+        public DetailsWeather temperatura = new DetailsWeather();
+        public DetailsWeather feels = new DetailsWeather();
+        public DetailsWeather tempMin = new DetailsWeather();
+        public DetailsWeather tempMax = new DetailsWeather();
+        public DetailsWeather presion = new DetailsWeather();
+        public DetailsWeather humedad = new DetailsWeather();
+        public DetailsWeather visibilidad = new DetailsWeather();
+        public DetailsWeather velocidad = new DetailsWeather();
+        public DetailsWeather sunr = new DetailsWeather();
+        public DetailsWeather suns = new DetailsWeather();
+        public DetailsWeather tmzone = new DetailsWeather();
+        public WeatherPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void WeatherPanel_Load(object sender, EventArgs e)
+        {
+            
+    
+            longitud.lblDetail.Text = "longitud";
+            flpContent.Controls.Add(longitud);
+
+            latitud.lblDetail.Text = "latitud";
+            flpContent.Controls.Add(latitud);
+
+            temperatura.lblDetail.Text = "Temperatura";
+            flpContent.Controls.Add(temperatura);
+
+            feels.lblDetail.Text = "feels_like";
+            flpContent.Controls.Add(feels);
+
+            tempMin.lblDetail.Text = "temp_min";
+            flpContent.Controls.Add(tempMin);
+
+            tempMax.lblDetail.Text = "temp_max";
+            flpContent.Controls.Add(tempMax);
+
+            presion.lblDetail.Text = "pressure";
+            flpContent.Controls.Add(presion);
+
+            humedad.lblDetail.Text = "humidity";
+            flpContent.Controls.Add(humedad);
+
+            visibilidad.lblDetail.Text = "visibility";
+            flpContent.Controls.Add(visibilidad);
+
+            velocidad.lblDetail.Text = "speed";
+            flpContent.Controls.Add(velocidad);
+
+            sunr.lblDetail.Text = "sunrise";
+            flpContent.Controls.Add(sunr);
+
+            suns.lblDetail.Text = "sunset";
+            flpContent.Controls.Add(suns);
+
+            tmzone.lblDetail.Text = "timezone";
+            flpContent.Controls.Add(tmzone);
+            
+        }
+
+    }
+}
